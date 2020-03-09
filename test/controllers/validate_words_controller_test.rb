2974144dd3a3bc6a7df645f387cd0af777ca9793
Ravi <ravi@Ravis-Mac-mini.local>
2020-03-09 20:48:04 +0545

    new tests

--- test/controllers/validate_words_controller_test.rb
+++ test/controllers/validate_words_controller_test.rb
@@ -1,12 +1,17 @@
 require 'test_helper'
+require 'json'
 
 class ValidateWordsControllerTest < ActionDispatch::IntegrationTest
-  test "should have file access" do
-    #get validate_words_validate_url
-    #assert_response :success
-    get "/validate"
-    assert_response :success 
-    #assert_equal 1, @controller.send(:resp_body)
+  test "should have return true" do
+    get "/validate?word=code", xhr: true
+    j = JSON.parse(@response.body)
+    assert_equal true, j['result']
+  end
+
+  test "should have return false" do
+    get "/validate?word=noword", xhr: true
+    j = JSON.parse(@response.body)
+    assert_equal false, j['result']
   end
 
 end
